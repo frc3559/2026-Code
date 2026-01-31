2ab74f07e19d84d3db1939f2519cb6f268e7070e
Emmett <emmetthurm@gmail.com>
2026-01-31 11:38:43 -0500

    Initialize IntakeSubsystem

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class IntakeSubsystem {
+    private SparkMax mainController;
+
+    public static final int  kIntakeMotorCanId = 20;
+    public IntakeSubsystem() {
+        mainController = new SparkMax(kIntakeMotorCanId, MotorType.kBrushless);
+    }
+}
